5f07985ebb31a04d99694d42629e71079ab5a30f
alexnrn2204 <alexnrn2204@gmail.com>
2025-11-19 21:18:41 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+from pathlib import Path
+
+
+def copy_file(full_command: str) -> None:
+    command_list = full_command.split()
+    if len(command_list) < 3:
+        return
+
+    if command_list[0] != "cp":
+        return
+
+    if not Path(command_list[1]).is_file():
+        return
+
+    if command_list[1] == command_list[2]:
+        return
+
+    with (open(command_list[1], "r") as file_from,
+          open(command_list[2], "w") as file_to):
+        file_to.write(file_from.read())
