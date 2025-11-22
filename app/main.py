426b2f3e24d2a6a9e3d6fb517181c0b3754ac85c
alexnrn2204 <alexnrn2204@gmail.com>
2025-11-22 18:23:18 +0200

    Solution 2

--- app/main.py
+++ app/main.py
@@ -3,18 +3,21 @@ from pathlib import Path
 
 def copy_file(full_command: str) -> None:
     command_list = full_command.split()
-    if len(command_list) < 3:
+    if len(command_list) != 3:
         return
 
     if command_list[0] != "cp":
         return
 
-    if not Path(command_list[1]).is_file():
+    source_path = command_list[1]
+    destination_path = command_list[2]
+
+    if not Path(source_path).is_file():
         return
 
-    if command_list[1] == command_list[2]:
+    if source_path == destination_path:
         return
 
-    with (open(command_list[1], "r") as file_from,
-          open(command_list[2], "w") as file_to):
+    with (open(source_path, "r") as file_from,
+          open(destination_path, "w") as file_to):
         file_to.write(file_from.read())
